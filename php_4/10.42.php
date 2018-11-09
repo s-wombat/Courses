1a5a5877a2a197f6cdba93a607b748eccafa3e15
Sergey Sinyushkin <serg.dnepr83@gmail.com>
2018-11-09 14:52:37 +0200

    new code

--- php_4/10.42.php
+++ php_4/10.42.php
@@ -10,31 +10,17 @@
 <?php
 
 //возведение в степень $n числа $a
-// function pw($a,$n) 
-// {
-// 	if($n > 0){
-// 		return $a*pw($a, $n-1);
-// 	}elseif($n < 0){
-// 		return $a*pw(1/$a, -$n);
-// 	}
-// 	return 1;
-// }
-// echo(pw(3,-2));
-$x=3;
-$n=-2;
-function rec($x,$n){
-  if($n==0){
-    return 1;
-  }
-  if($n > 0){
-    return $x * rec($x,$n - 1);
-  }
-  if($n < 0){
-    return $x * rec(1/$x, -$n);
-  }
+function pw($a,$n) 
+{
+	if($n > 0){
+		return $a*pw($a, $n-1);
+	}elseif($n < 0){
+		return $a*pw(1/$a, -($n)); // отрицательная степень не решена
+	}
+	return 1;
 }
-$y=rec($x,$n);
-echo $y;
+echo(pw(3,-2));
+
 ?>
 </body>
 </html>
